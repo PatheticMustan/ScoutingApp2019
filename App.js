b9bef9c13b77a705e7241192cd19fcbe75c36314
KryptoCrash <kryptocrashmh@gmail.com>
2020-02-10 17:21:13 -0500

    header container

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style = {styles.headerContainer}>
+					<View style = {styles.headerTextContainer}>
+						<Text style = {styles.headerText}>2020 - Infinite Recharge</Text>
+					</View>
+					<View style = {styles.optionContainer}>
+
+					</View>
+				</View>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff'
+	},
+	headerContainer: {
+		flexDirection: "column",
+		paddingVertical: 15,
+		backgroundColor: "#ddd",
+		alignItems: "center"
+	},
+	headerTextContainer: {
+		flex: 1,
+		flexDirection: "row"
+	},
+	headerText: {
+		flex: 1,
+		fontSize: 30,
+		textAlign: "center"
+	}
 });
