8d314666233617ad203ff7f3d2b9286bc044cef4
PatheticMustan <patheticmustan@gmail.com>
2020-02-13 15:39:42 -0500

    thing

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-	StyleSheet,
 	View
 } from 'react-native';
 
@@ -17,10 +16,3 @@ export default class App extends React.Component {
 		);
 	}
 }
-
-const styles = StyleSheet.create({
-	container: {
-		borderWidth: StyleSheet.hairlineWidth,
-		borderColor: "#000"
-	}
-});
