5cb85b1c5d3d3d06e5c821599bfa08e490fa21ef
PatheticMustan <patheticmustan@gmail.com>
2020-02-13 17:29:09 -0500

    Rename header

---
+++ components/Header.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+	StyleSheet,
+	Text,
+	View
+} from 'react-native';
+import * as Sharing from 'expo-sharing';
+import Link from './Utility/Link.js';
+
+export default class Header extends React.Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.headerText}>2020 - Infinite Recharge</Text>
+
+				<View style={styles.linkContainer}>
+					<Link color="red" onPress={() => alert(1)}>Reset</Link>
+					<Link color="blue" onPress={() => alert(1)}>Undo</Link>
+					<Link color="blue" onPress={() => alert(1)}>Reset</Link>
+					<Link color="blue" onPress={() => alert(1)}>Save</Link>
+					<Link color="blue" onPress={() => Sharing}>Save and Export</Link>
+				</View>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#DDD'
+	},
+	headerText: {
+		flex: 1,
+		fontSize: 30,
+		paddingTop: 10,
+		textAlign: "center"
+	},
+	linkContainer: {
+		flex: 1,
+		flexDirection: "row",
+		justifyContent: "space-between",
+		paddingHorizontal: 30,
+		paddingBottom: 15,
+		paddingTop: 10
+	}
+});
