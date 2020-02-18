a43d0058b8f08f5021edfd9c7b8de7c062960631
Aidan Din <00100000spacebar@gmail.com>
2020-02-18 12:11:50 -0500

    my dumb way of making a button red

--- components/Other.js
+++ components/Other.js
@@ -3,18 +3,42 @@ import {
 	StyleSheet,
 	Text,
     View,
+    TouchableWithoutFeedback
 } from 'react-native';
 
 import BoolButton from './Buttons/BoolButton.js';
 
 export default class Other extends React.Component {
+    state = {i: 0}
+    switchValue = (i) => {
+        if (i == 0) {
+            this.setState({i: i + 1})
+        } else {
+            this.setState({i: i - 1})
+        }
+    }
 	render() {
+        const p = this.state.i;
+
 		return (
             <View style = {styles.container}>
                 <Text style = {{textAlign: "center", fontSize: 35, fontWeight: 'bold'}}>Other</Text>
                 <View style = {styles.otherContainer}>
                     <BoolButton>Yellow Card</BoolButton>
-                    <BoolButton>Red Card</BoolButton>
+                    <TouchableWithoutFeedback onPress = {() => this.switchValue(this.state.i)}>
+                        <View style = {{
+                            textAlign: 'center',
+                            justifyContent: 'center',
+                            borderRadius: 10,
+                            borderWidth: StyleSheet.hairlineWidth,
+                            borderColor: '#000000',
+                            width: (p.width? p.width : 100),
+                            height: (p.height? p.height : 40),
+                            backgroundColor: (this.state.i? 'red' : 'white')
+                        }}>
+                        <Text>Red Card</Text>
+                    </View>
+                </TouchableWithoutFeedback>
                 </View>
             </View>
 		);
