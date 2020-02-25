04cea59bc80c664c117761fb3a6e6dda983c7cd5
PatheticMustan <patheticmustan@gmail.com>
2020-02-24 23:06:35 -0500

    wtf

--- Routes/PastMatches.js
+++ Routes/PastMatches.js
@@ -8,7 +8,7 @@ import Header from './ScoutComponents/Header.js';
 import MatchInfoContainer from './ScoutComponents/MatchInfoContainer.js';
 import Autonomous from './ScoutComponents/Autonomous.js';
 import TeleOp from './ScoutComponents/TeleOp.js';
-import Endgame from './ScoutComponents/Endgame.js';
+import Endgame from './ScoutComponents/EndGame.js';
 import Other from './ScoutComponents/Other.js';
 
 import Data from './ScoutComponents/Data.js';
