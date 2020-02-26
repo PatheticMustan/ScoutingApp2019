6c7b8f2a290ac5f6b58bc3494e390566cb011360
PatheticMustan <patheticmustan@gmail.com>
2020-02-26 15:05:16 -0500

    timer

--- Routes/ScoutComponents/Utility/Timer.js
+++ Routes/ScoutComponents/Utility/Timer.js
@@ -1,26 +1,79 @@
 import React from 'react';
 import {
-    View
+    View,
+    Text,
+    StyleSheet,
+    TouchableWithoutFeedback
 } from 'react-native';
-
 import BoolButton from '../Buttons/BoolButton.js';
 
 export default class Timer extends React.Component {
-    /** Update */
-    componentDidMount() {
-        /** Set default */
-        global.data[this.props.id] = this.props.default;
+    constructor(props) {
+        super(props);
 
-        this.interval = setInterval(() => {this.setState({ time: Date.now() })}, 20);
-        this.selectMatch = (v, i) => global.data[this.props.id] = v;
+        this.state = {
+            start: false,
+            sec: 0
+        }
+
+        this.time = "0:00";
     }
-    componentWillUnmount() {clearInterval(this.interval)}
+
+    timerClick = () => {
+        if (this.state.start == false) {
+            this.interval = setInterval(() => {
+                this.setState({
+                    start: true,
+                    sec: this.state.sec + 1
+                });
+
+                const s = this.state.sec + 1;
+                this.time = (`${(s-(s%60))/60}:${(s % 60)}`)
+            }, 1000);
+        } else {
+            clearInterval(this.interval);
+            this.setState({
+                start: false
+            });
+        }
+    };
+
+    onResetTimer = () => {
+        this.setState ({
+            sec: 0,
+            start: false
+        });
+    }
     
     render() {
         return (
             <View>
-                <BoolButton id="timerOn">{this.props.children}</BoolButton>
+                <View>
+                    <Text>{this.time}</Text>
+                </View>
+
+                <BoolButton
+                    id="TimerClicked"
+                    bgc="lime"
+                    width={160}
+                    onPress={this.timerClick}
+                >
+                        {this.state.start? "Stop" : "Start"} Stopwatch
+                </BoolButton>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    timerButton: {
+        height: 40,
+        width: 160,
+        textAlign: "center",
+        justifyContent: "center",
+        borderWidth: StyleSheet.hairlineWidth,
+        borderRadius: 10,
+        borderColor: 'black',
+        backgroundColor: 'white'
+    },
+})
