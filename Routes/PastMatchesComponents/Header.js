98ab386404622d2eae5b9b6898fbd771c54c461a
Aidan Din <00100000spacebar@gmail.com>
2020-02-27 15:18:46 -0500

    Headers set up

---
+++ Routes/PastMatchesComponents/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+    View
+} from 'react-native';
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <View>
+            </View>
+        );
+    }
+}
--- Routes/Settings.js
+++ Routes/Settings.js
@@ -4,31 +4,11 @@ import {
     ScrollView
 } from 'react-native';
 
-import Header from './ScoutComponents/Header.js';
-import MatchInfoContainer from './ScoutComponents/MatchInfoContainer.js';
-import Autonomous from './ScoutComponents/Autonomous.js';
-import TeleOp from './ScoutComponents/TeleOp.js';
-import Endgame from './ScoutComponents/EndGame.js';
-import Other from './ScoutComponents/Other.js';
-
-import Data from './ScoutComponents/Data.js';
-
-/** Test Components */
-
 export default class Settings extends React.Component {
     render() {
         return (
             <ScrollView>
                 <View>
-                    <Data/>
-
-                    <Header/>
-                    
-                    <MatchInfoContainer/>
-                    <Autonomous/>
-                    <TeleOp/>
-                    <Endgame/>
-                    <Other/>
                 </View>
             </ScrollView>
         );
