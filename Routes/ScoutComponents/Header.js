49b9e943422a5e3d408cbfdaded34aa84c717928
PatheticMustan <patheticmustan@gmail.com>
2020-03-01 01:41:40 -0500

    Storage begin
    now let the storage games being

--- Routes/ScoutComponents/Header.js
+++ Routes/ScoutComponents/Header.js
@@ -7,9 +7,17 @@ import {
 import * as Sharing from "expo-sharing";
 import Link from "../../Components/Utility/Link.js";
 import SegmentedControl from "../../Components/Buttons/SegmentedControl.js";
+import {localStorage} from '../../Components/Storage/Storage.js';
 
 
 export default class Header extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.storage = localStorage;
+        alert(typeof this.storage);
+    }
+
     render() {
         return (
             <View style={styles.container}>
@@ -30,7 +38,9 @@ export default class Header extends React.Component {
                         default={0}
                     />
                     
-                    <Link color="blue" onPress={() => alert(1)}>Save</Link>
+                    <Link color="blue" onPress={() => {
+                        localStorage
+                    }}>Save</Link>
                     <Link color="blue" onPress={() => Sharing}>Save and Export</Link>
                 </View>
             </View>
