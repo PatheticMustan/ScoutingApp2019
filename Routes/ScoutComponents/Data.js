ee53c328687d49e3ed00700f905f2b7a911986f9
PatheticMustan <patheticmustan@gmail.com>
2020-03-01 16:41:50 -0500

    fuck this shit

--- Routes/ScoutComponents/Data.js
+++ Routes/ScoutComponents/Data.js
@@ -63,6 +63,50 @@ export default class Data extends React.Component {
         global.output = output;
     }
 
+
+
+    reset() {
+        global.data = {
+            Team: 0,
+            MatchType: "Qualification",
+            BallsPickedUp: 0,
+            CrossesInitiationLine: false,
+            LinePosition: "",
+            AutoLow: 0,
+            AutoOuter: 0,
+            AutoInner: 0,
+            AutoMissed: 0,
+            PlaysDefense: false,
+            BallsPickedUpFromLoadingStation: 0,
+            Rotation: false,
+            Color: false,
+            ShootFrom: "",
+            BallsPickedUpFromFloor: 0,
+            FitsUnderTrench: false,
+            TeleLow: 0,
+            TeleOuter: 0,
+            TeleInner: 0,
+            TeleMissed: 0,
+            BallsScored: 0,
+            EndgameType: 2,
+            Time: "0:00",
+            TimerClicked: false,
+            ClimbHeight: "",
+            ClimbPosition: "",
+            YellowCard: false,
+            RedCard: false,
+            TeamNumber: "",
+            MatchNumber: 0,
+            Scouters: "",
+            StartingPieces: 0,
+            AutonomousComments: "",
+            TeleopComments: "",
+            EndgameComments: "",
+        }
+    }
+
+
+
     render() {
         return (
             <View>
--- Routes/ScoutComponents/Header.js
+++ Routes/ScoutComponents/Header.js
@@ -2,7 +2,8 @@ import React from "react";
 import {
     StyleSheet,
     Text,
-    View
+    View,
+    Alert
 } from "react-native";
 import * as Sharing from "expo-sharing";
 import Link from "../../Components/Utility/Link.js";
@@ -28,13 +29,38 @@ export default class Header extends React.Component {
     componentDidMount() {this.interval = setInterval(() => {this.setState({ time: Date.now() })}, 100)}
     componentWillUnmount() {clearInterval(this.interval)}
 
+    reset() {
+        Alert.alert(
+            "Reset",
+            "Are you sure you want to reset the Scoutsheet?",
+            [
+                {text: 'Reset', onPress: () => console.log('DEV TODO RESET')},
+                {text: 'Cancel', style: 'cancel'}
+            ]
+          );
+    }
+
+    save() {
+        (async () => {
+            await this.storage.get("meta")
+            await this.storage.set("test", "tesssssa testorosa");
+            alert(await this.storage.get("test"));
+        })()
+        //
+    }
+
+    saveAndExport() {
+        this.save();
+        // export
+    }
+
     render() {
         return (
             <View style={{backgroundColor: !global.data["Team"]? "#D0F4FF" : "#FFD0D0"}}>
                 <Text style={styles.headerText}>2020 - Infinite Recharge</Text>
 
                 <View style={styles.linkContainer}>
-                    <Link color="red" onPress={() => alert(1)}>Reset</Link>
+                    <Link color="red" onPress={() => this.reset()}>Reset</Link>
                     
                     <SegmentedControl
                         id="Team"
