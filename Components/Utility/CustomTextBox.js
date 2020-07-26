3286a4367e96b52182faae5a4fdbcf7e7b7cf098
Mustan Pathetic <patheticmustan@gmail.com>
2020-07-26 16:07:13 -0400

    refactor customTextBox????
    I'm worried of performance, and it looks like I was right to worry. Performance is already garbage on web, I'm afraid of how it'll be on mobile.

--- Components/Utility/CustomTextBox.js
+++ Components/Utility/CustomTextBox.js
@@ -5,60 +5,53 @@ import {
 	StyleSheet
 } from "react-native";
 
+import { setKeyPair, setDefault, selectData } from "../../Redux/Features/dataSlice.js";
+import { useDispatch, useSelector } from "react-redux";
 
+export default function CustomTextBox(props) {
+	const dispatch = useDispatch();
 
-export default class CustomTextBox extends React.Component {
-	constructor(props) {
-		super(props);
-		/** Set default */
-		this.state = {
-			text: global.data[props.id],
-			cm: -1
-		};
-		global.data[props.id] = this.state.text;
-	}
-	componentDidMount() {
-		this.matchUpdate = setInterval(() => {
-			if (global.currentMatchID != this.state.cm) {
-				this.setState({
-					text: global.data[this.props.id],
-					cm: global.currentMatchID
-				});
-			}
-		}, 500);
-	}
-	render() {
-		return (
-			<View style={{
-				width: this.props.width,
-				height: this.props.height || 40
-			}}>
-				<TextInput
-					multiline={this.props.multi}
-					numberOfLines={this.props.multi? this.props.lines : 1}
-					editable
-					placeholder={this.props.placeholder || ""}
-					value={this.state.text}
-					style={{
-						flex: 1,
-						padding: 10,
-						backgroundColor: (this.props.backgroundColor? this.props.backgroundColor : "#FFFFFF"),
-						borderColor: "#666",
-						borderWidth: StyleSheet.hairlineWidth,
-						borderRadius: (this.props.borderRadius? this.props.borderRadius : this.props.height / 5)
-					}}
-					{...this.props.options}
+	// set default value
+	dispatch(setDefault([props.id, ""]));
 
-					onChangeText={text => this.setState(
-						{text: text
-							.replace(/\n/g, " ")
-							.replace(/,/g, " ")
-							/** pretty sure there are many more instances. */
-						},
-						() => {global.data[this.props.id] = this.state.text;}
-					)}
-				/>
-			</View>
-		);
-	}
+	// get value from store
+	const kpv = useSelector(selectData);
+	const text = kpv.find(v => v[0] === props.id)[1];
+
+	return (
+		<View style={{
+			width: props.width,
+			height: props.height || 40
+		}}>
+			<TextInput
+				multiline={props.multi}
+				numberOfLines={props.multi? props.lines : 1}
+				editable
+				placeholder={props.placeholder || ""}
+				value={text}
+				style={{
+					flex: 1,
+					padding: 10,
+					backgroundColor: (props.backgroundColor? props.backgroundColor : "#FFFFFF"),
+					borderColor: "#666",
+					borderWidth: StyleSheet.hairlineWidth,
+					borderRadius: (props.borderRadius? props.borderRadius : props.height / 5)
+				}}
+				{...props.options}
+
+				onChangeText={text => {
+					// TODO: instead of just filtering text, encode it!
+					let filtered = text
+						.replace(/\n/g, " ")
+						.replace(/,/g, " ");
+
+					// dispatch to redux
+					dispatch(setKeyPair([props.id, filtered]));
+					
+					console.log(filtered);
+				}}
+			/>
+		</View>
+	);
+
 }
