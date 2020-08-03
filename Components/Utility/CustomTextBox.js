bdcf944c1ce3ade56f969896460552170f83fd97
Mustan Pathetic <patheticmustan@gmail.com>
2020-08-03 01:06:17 -0400

    Fix #36
    https://github.com/PatheticMustan/ScoutingApp2019/issues/36#issuecomment-667728272

--- Components/Utility/CustomTextBox.js
+++ Components/Utility/CustomTextBox.js
@@ -12,6 +12,9 @@ export default function CustomTextBox(props) {
 	const dispatch = useDispatch();
 
 	const [text, setText] = useState("");
+	// Fixes issue #36
+	// https://github.com/PatheticMustan/ScoutingApp2019/issues/36#issuecomment-667728272
+	const [watchdog, bark] = useState("");
 
 	// set default value
 	dispatch(setDefault([props.id, ""]));
@@ -22,9 +25,30 @@ export default function CustomTextBox(props) {
 
 	useEffect(() => {
 		const interval = setInterval(() => {
-			console.log("ugh, " + reduxText);
-			if (reduxText !== text) dispatch(setKeyPair([props.id, text]));
-		}, 1000);
+			/** #36 and solution:
+			 * CONTEXT:
+			 * every second, we want to check if the user typed.
+			 * if they DID type, text should be different. We can check with (text !== reduxText)
+			 * 
+			 * PROBLEM:
+			 * now the problem is we can't update text with reduxText, since it thinks the user typed.
+			 * 
+			 * SOLUTION:
+			 * A way to fix this is to have a different state (watchdog) tracking specifically if reduxText changed.
+			 * reduxText only updates when the textbox updates it, or during loadMatch!
+			 * watchdog only updates when the textbox updates it.
+			 * So we can kinda subtract the two. Now, we can check if loadMatch happened!
+			 * We can check this with (watchdog !== reduxText)
+			 **/
+			if (watchdog !== reduxText)  {
+				setText(reduxText);
+			} else {
+				if (reduxText !== text) {
+					bark(text);
+					dispatch(setKeyPair([props.id, text]));
+				}
+			}
+		}, 500);
 		
 		// basically componentWillUnmount but this time it's for React hooks
 		return () => clearInterval(interval);
